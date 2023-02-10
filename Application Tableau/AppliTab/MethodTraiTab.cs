c7a383298a86b908a789ac173851e825501726ea
Grootaers Romane <210469@CCM-Asty-Moulin.local>
2023-02-10 16:16:28 +0100

    rien

---
+++ Application Tableau/AppliTab/MethodTraiTab.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppliTab
+{
+    public struct MethodTraiTab
+    {
+        public void GenereTab(out int[] tAleat, int nbrV, int borneDIntervalles, int borneFIntervalles)
+        {
+            Random nAleat = new Random();
+            
+            tAleat = new int[nbrV];
+
+            for (int compteur = 0; compteur <= nbrV - 1; compteur++)
+            {
+                tAleat[compteur] = nAleat.Next(borneDIntervalles, borneFIntervalles + 1);
+            }
+        }
+        public void AfficherTab(int[] tab, out string resultat)
+        {
+            resultat = "";
+
+            for (int compteur = 0; compteur <= tab.Length - 1; compteur++)
+            {
+                resultat = resultat + tab[compteur] + " ";
+            }
+            
+        }
+        public void AfficherTabS(string[] tab, out string resultat)
+        {
+            resultat = "";
+
+            for (int compteur = 0; compteur <= tab.Length - 1; compteur++)
+            {
+                resultat = resultat + tab[compteur] + " ";
+            }
+
+        }
+        public void FonctionMixed(int[] tJours, string[] tMois, out string[] tMixed)
+        {
+            int j = 0;
+            tMixed = new string[24];
+            for (int compteur = 0; compteur < (tJours.Length)*2 - 1; compteur = compteur + 2)
+            {
+                tMixed[compteur] = tMois[j];
+                tMixed[compteur+1] = tJours[j].ToString(); //Méthode pour convertir n'importe quel type en string
+                j++;
+            }
+        }
+
+        public void TabClassement(int[] tPositifRempli, out int[] tImpair, out int[] tPair)
+        {
+            tPair = new int[20];
+            tImpair = new int[20];
+            int j = 0;
+            int n = 0;
+            for (int i = 0; i <= 20 - 1; i++)
+            {
+                if (tPositifRempli[i] % 2 == 0)
+                {
+                    tPair[j] = tPositifRempli[i];
+                    j++;
+                }
+                else
+                {
+                    tImpair[n] = tPositifRempli[i];
+                    n++;
+                }
+            }
+            while (j != 20 - 1)
+            {
+                tPair[j] = 0;
+                j++;
+            }
+            while (n != 20 - 1)
+            {
+                tImpair[n] = 0;
+                n++;
+            }
+        }
+        public void TabFibonacci(out int[] tFibo, int n)
+        {
+            tFibo = new int[n - 1];
+            int i = 2;
+            tFibo[0] = 1;
+            tFibo[1] = 1;
+            while (i != n)
+            {
+                tFibo[i] = tFibo[i - 1] + tFibo[i - 2];
+                i++;
+            }
+        }
+        public void TabDecal(ref int[] tabI, int val)
+        {
+            int nbOcc = 0;
+            for (int i = 0; i < tabI.Length; i++)
+            {
+                if (tabI[i] == val)
+                {
+                    nbOcc++;
+                }
+                else
+                {
+                    tabI[i - nbOcc] = tabI[i];
+                }
+            };
+            for (int i = tabI.Length - nbOcc; i < tabI.Length; i++)
+            {
+                tabI[i] = val;
+            }
+        }
+    }
+}
---
+++ Application Tableau/AppliTab/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace AppliTab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodTraiTab tools = new MethodTraiTab();
+            int nbrV;
+            int[] tAleat;
+            string resultat;
+            int borneDIntervalles;
+            int borneFIntervalles;
+            int[] tJours = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            string[] tMois = new string[] {"Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"};
+            string[] tMixed;
+            int answer;
+            int[] tPositifRempli;
+            int[] tImpair;
+            int[] tPair;
+            int n;
+            int[] tFibo;
+            int[] tabI;
+            int val;
+
+            Console.WriteLine("Choisissez l'option que vous voulez(1, 2, 3, 4 ou 5)");
+            answer = int.Parse(Console.ReadLine());
+
+            if (answer == 1)
+            {
+                Console.WriteLine("Choisissez la longueur de tableau que vous désirez");
+                nbrV = int.Parse(Console.ReadLine());
+                Console.WriteLine("Choisissez la valeure par laquelle vous voulez commencer votre tableau");
+                borneDIntervalles = int.Parse(Console.ReadLine());
+                Console.WriteLine("Choisissez la valeure par laquelle vous voulez finir votre tableau");
+                borneFIntervalles = int.Parse(Console.ReadLine());
+
+                tools.GenereTab(out tAleat, nbrV, borneDIntervalles, borneFIntervalles);
+                tools.AfficherTab(tAleat, out resultat);
+                Console.WriteLine(resultat);
+            }
+            else if (answer == 2)
+            {
+                tools.FonctionMixed(tJours, tMois, out tMixed);
+                tools.AfficherTabS(tMixed, out resultat);
+                Console.WriteLine(resultat);
+            }
+            else if (answer == 3)
+            {
+                tools.GenereTab(out tPositifRempli, 20, 0, 100);
+                tools.TabClassement(tPositifRempli, out tImpair, out tPair);
+                tools.AfficherTab(tPositifRempli, out resultat);
+                tools.AfficherTab(tImpair, out resultat);
+                tools.AfficherTab(tPair, out resultat);
+                Console.WriteLine(resultat);
+            }
+            else if (answer == 4)
+            {
+                Console.WriteLine("Choisissez le nombre de valeurs que composera cette suite");
+                n = int.Parse(Console.ReadLine());
+                tools.TabFibonacci(out tFibo, n);
+            }
+            else if (answer == 5)
+            {
+                Console.WriteLine("Choisissez la longueur de tableau que vous désirez");
+                nbrV = int.Parse(Console.ReadLine());
+                Console.WriteLine("Choisissez la valeure par laquelle vous voulez commencer votre tableau");
+                borneDIntervalles = int.Parse(Console.ReadLine());
+                Console.WriteLine("Choisissez la valeure par laquelle vous voulez finir votre tableau");
+                borneFIntervalles = int.Parse(Console.ReadLine());
+
+                tools.GenereTab(out tabI, nbrV, borneDIntervalles, borneFIntervalles);
+                tools.AfficherTab(tabI, out resultat);
+                Console.WriteLine(resultat);
+                Console.WriteLine("Choisissez une valeur indésirable dans votre tableau");
+                val = int.Parse(Console.ReadLine());
+                tools.TabDecal(ref tabI, val);
+                tools.AfficherTab(tabI, out resultat);
+                Console.WriteLine(resultat);
+            }
+        }
+    }
+}
