4f3aadeb6878715eff9f13d718f4a87a48729e2d
Grootaers Romane <210469@CCM-Asty-Moulin.local>
2023-04-18 12:19:44 +0200

    interro matrices

---
+++ IMATP5_GROOTAERS/IMATP5_GROOTAERS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IMATP5_GROOTAERS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            outils tools = new outils();
+            string chaine;
+            int N;
+            int[,] maTriP;
+            string chainC;
+
+            Console.WriteLine("Donnez la taille de la matrice :");
+            N = int.Parse(Console.ReadLine());
+            tools.TriMatP(out maTriP, N, out chaine);
+            tools.ChainC(maTriP, N, out chainC);
+        }
+    }
+}
---
+++ IMATP5_GROOTAERS/IMATP5_GROOTAERS/outils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMATP5_GROOTAERS
+{
+    class outils
+    {
+        public void TriMatP(out int[,] maTriP, int N, out string chaine)
+        {
+            int resultat;
+            chaine = "";
+            maTriP = new int[N, N];
+            int compteur = 1;
+            for (int Nlignes = 0; Nlignes <= maTriP.GetLength(0) - 1; Nlignes++)
+            {
+                for (int Ncolonnes = 0; Ncolonnes <= maTriP.GetLength(1) - 1; Ncolonnes++)
+                {
+                    maTriP[Nlignes, Ncolonnes] = 0;
+                    maTriP[Nlignes, 0] = 1;
+                    while (maTriP[Nlignes, Ncolonnes] != 0)
+                    {
+                        resultat = maTriP[Nlignes, Ncolonnes] + maTriP[Nlignes, Ncolonnes + compteur];
+                        maTriP[Nlignes + compteur, Nlignes + compteur] = resultat;
+                    }
+                    compteur++;
+                    if (maTriP[Nlignes, Ncolonnes] == 0)
+                    {
+                        maTriP[Nlignes + 1, Ncolonnes] = 0;
+                    }
+                    chaine = chaine + maTriP[Nlignes, Ncolonnes];
+                }
+            }
+        }
+        public void ChainC(int[,] maTriP, int N, out string chainC)
+        {
+            chainC = "";
+            int compteur = 0;
+            for (int Nlignes = 0; Nlignes <= maTriP.GetLength(0) - 1; Nlignes++)
+            {
+                for (int Ncolonnes = 0; Ncolonnes <= maTriP.GetLength(1) - 1; Ncolonnes++)
+                {
+                    string terme = maTriP[Nlignes.GetLength(0), Ncolonnes++] + "a" ^ N - compteur + "b" ^ compteur;
+                    chainC = chainC + terme;
+                }
+                chainC = chainC + "+";
+            }
+        }
+    }
+}
